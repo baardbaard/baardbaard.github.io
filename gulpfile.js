92fd73c5369fbc61ae4f0477ae91487fa7408610
Stef Thoen <stef@baardbaard.nl>
2015-05-21 15:12:48 +0200

    Big cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var reload = browserSync.reload;
 
 var paths = {
 	scss: ['_/scss/*.scss', '_/scss/**/*.scss'],
-	svg: ['_/img/svg/*.svg']
-
+	svg: '_/img/svg/*.svg',
+	html: '*.html'
 };
 
 gulp.task('browser-sync', function() {
@@ -22,31 +22,36 @@ gulp.task('scss', function() {
 	gulp.src(paths.scss)
 		.pipe(sass())
 		.pipe(gulp.dest('_/css'))
-		.pipe(reload({stream: true}));
+		.pipe(browserSync.stream());
 });
 
 gulp.task('svg', function() {
 	gulp.src(paths.svg)
 	.pipe(svgSprite({
 		mode: {
-			css: { // Activate the «css» mode
+			css: { 
 				bust: false,
 				render: {
-					css: true // Activate CSS output (with default options)
+					css: true
 				},
 				example: true
 			}
-		}
+		},
+		transform: [{ 
+			svgo: {
+				plugins: [
+					{ mergePaths: false } // SVGO doesn't like my SVG's
+				]
+			}
+		}]
 	}))
 	.pipe(gulp.dest('_/out'));
-});
-
-gulp.task('default', function() {
 });
 
 gulp.task('watch', function() {
 	gulp.watch(paths.scss, ['scss']);
 	gulp.watch(paths.svg, ['svg']);
+	gulp.watch(paths.html).on('change', reload);
 });
 
 gulp.task('default', ['watch', 'scss', 'svg', 'browser-sync']);
